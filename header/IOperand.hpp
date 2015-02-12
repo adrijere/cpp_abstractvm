d12d2477045265a9b350ba7b179d1fcaee4ffa9a
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-02-12 13:09:17 +0100

    Ajout de propriétés dans la classe de l'IOperand

--- header/IOperand.hpp
+++ header/IOperand.hpp
@@ -4,7 +4,16 @@
 # include <iostream>
 
 class IOperand {
+ private:
+	// IOperand *(IOperand::*funct)(const std::string &)[5]; // Tableau qui servira de map pour pointeur sur membres.
 
+	IOperand *createInt8(const std::string &);
+	IOperand *createInt16(const std::string &);
+	IOperand *createInt32(const std::string &);
+	IOperand *createFloat(const std::string &);
+	IOperand *createDouble(const std::string &);
+
+ public:
 	enum eOperandType {
 		Int8,
 		Int16,
@@ -13,7 +22,8 @@ class IOperand {
 		Double
 	};
 
- public:
+	IOperand *createOperand(eOperandType, const std::string &);
+
 	virtual std::string const &toString(void) const = 0;
 
 	virtual int getPrecision(void) const = 0;
