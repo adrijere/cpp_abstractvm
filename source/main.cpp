91f15027eb035e789615f9b24279dc763520337e
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-02-18 18:59:07 +0100

    Ajout d'un main pour tester

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,43 @@
 * @Author: gicque_p
 * @Date:   2015-02-12 09:59:22
 * @Last Modified by:   gicque_p
-* @Last Modified time: 2015-02-18 16:05:29
+* @Last Modified time: 2015-02-18 18:58:04
 */
 
 #include "Parsing.hpp"
 #include "Core.hpp"
 #include "Hatchery.hpp"
 #include "Memory.hpp"
+#include "Error.hpp"
+#include <map>
 
-int main() {	
+int main() {
+	Core core;
+
+	std::string cmd;
+	std::map<std::string, void (Core::*)(IOperand *)> map;
+
+	map["push"] = &Core::push;
+	map["dump"] = &Core::dump;
+	map["print"] = &Core::print;
+	map["add"] = &Core::add;
+	map["pop"] = &Core::pop;
+
+	Hatchery hatchery;
+	IOperand *operand = hatchery.createOperand(Int8, "42");
+
+	while (std::cin >> cmd) {
+		if (map[cmd]) {
+			try {
+				(core.*map[cmd])(operand);			
+			} catch (const CoreError &error) {
+				std::cerr << error.what() << std::endl;
+				return -1;
+			}
+		} else {
+			std::cout << "Instruction non reconnue" << std::endl;
+		}
+	}
+
     return 0;
 }
